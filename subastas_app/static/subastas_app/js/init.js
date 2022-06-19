2a9a06ab92ff7925d5b66b9d8d5b2d9d529012b6
Nestor Juarez <nesk.company@gmail.com>
2022-06-19 18:17:55 -0500

    Se corrigen datepicker y formato de barra de navegacion

--- subastas_app/static/subastas_app/js/init.js
+++ subastas_app/static/subastas_app/js/init.js
@@ -3,6 +3,22 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $(".dropdown-trigger").dropdown({
+        hover: false,
+        coverTrigger: false,
+        alignment: 'center',
+        constrainWidth: true
+    });
+    $('.datepicker').datepicker({
+      firstDay: 1,
+      selectYears: 90,
+      format: 'yyyy-mm-dd',
+      yearRange: [1910, 2022]
+  });
+  $('.slider').slider({
+    indicators: false,
+    height: 500
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
